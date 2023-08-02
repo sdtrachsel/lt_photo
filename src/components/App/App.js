53ec1ca2c12ad614d92f7e431cbfa57ad6d9e1d1
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-08-02 11:39:52 -0500

    Add Route for logo to /"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements
+ } from "react-router-dom";
+
+import { Album } from "../Album/Album";
+import { Albums } from "../Albums/Albums";
+import { RootLayout } from "../../layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} >
+      <Route path="/" element={<Albums />} />
+      <Route path="album" element={<Album />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="text-xl font-bold">
-      <h1>App</h1>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
