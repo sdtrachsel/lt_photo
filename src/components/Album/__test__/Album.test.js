5c666d05e97d4b107cf3fc0c585f6949d17fd7c9
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-08-07 13:28:18 -0500

    Add Album test

---
+++ src/components/Album/__test__/Album.test.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter, useParams } from "react-router-dom";
+import { Album } from "../Album";
+import { getAlbum } from "../../../apiCalls";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest.fn(),
+}));
+
+jest.mock("../../../apiCalls", () => ({
+  getAlbum: jest.fn(),
+}));
+
+jest.mock("../../Loader/Loader", () => ({
+  Loader: () => {
+    return <div>Loading</div>
+  }
+}));
+
+jest.mock("../../PhotoCard/PhotoCard", () => ({
+  PhotoCard: () => {
+    return <div>PhotoCard</div>
+  }
+}));
+
+jest.mock("../../Error/Error", () => ({
+  Error: () => {
+    return <div>Error</div>
+  }
+}));
+
+const MockAlbum = () => {
+  return (
+    <BrowserRouter>
+      <Album />
+    </BrowserRouter>
+  )
+};
+
+describe("Album component", () => {
+  const mockAlbumId = "1";
+
+  beforeEach(() => {
+    useParams.mockReturnValue({ albumId: mockAlbumId });
+  });
+
+  it("should show loader when loading", async () => {
+    getAlbum.mockResolvedValueOnce([]);
+    render(MockAlbum());
+    expect(screen.getByText("Loading")).toBeInTheDocument();
+  });
+
+  it("should show error when fetch fails", async () => {
+    const mockErrorMsg = "Error fetching album";
+    getAlbum.mockRejectedValueOnce({ message: mockErrorMsg });
+    render(MockAlbum());
+    await waitFor(() => {
+      expect(screen.getByText("Error")).toBeInTheDocument();
+    });
+  });
+
+  it(" should show PhotoCard components when fetch succeeds", async () => {
+    const mockPhotos = [
+      {
+        id: 1,
+        title: "photo1",
+        thumbnailUrl: "http://example.com/photo1.jpg",
+        url: "http://example.com/photo1.jpg",
+      },
+      {
+        id: 2,
+        title: "photo2",
+        thumbnailUrl: "http://example.com/photo2.jpg",
+        url: "http://example.com/photo2.jpg",
+      },
+    ];
+    getAlbum.mockResolvedValueOnce(mockPhotos);
+    render(MockAlbum());
+    await waitFor(() => {
+      expect(screen.getAllByText("PhotoCard")).toHaveLength(mockPhotos.length);
+    });
+  });
+});
--- src/components/Error/__test__/Error.test.js
+++ src/components/Error/__test__/Error.test.js
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react';
 import { Error } from '../Error';
 
 describe('Error component', () => {
-  test('renders "Opppsss..."', () => {
+  it('renders "Opppsss..."', () => {
     render(<Error />);
 
     const headingElement = screen.getByText(/Opppsss.../i);
     expect(headingElement).toBeInTheDocument();
   });
 
-  test('renders custom error message when passed as prop', () => {
+  it('renders custom error message when passed as prop', () => {
     const customMessage = 'Custom Error Message';
 
     render(<Error errorMsg={customMessage} />);
@@ -18,7 +18,7 @@ describe('Error component', () => {
     expect(errorMessage).toBeInTheDocument();
   });
 
-  test('renders default error message when no prop provided', () => {
+  it('renders default error message when no prop provided', () => {
     const defaultMessage = 'We are currently experiencing issues. Please try again later';
 
     render(<Error />);
