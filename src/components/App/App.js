d1b4eca4c61b6853ce3bdf614a0edcd3a5b405b4
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-08-08 18:58:21 -0500

    Refactor to React Router v6.14.2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Header } from "../Header/Header";
 import { Album } from "../Album/Album";
 import { Albums } from "../Albums/Albums";
@@ -9,12 +9,12 @@ function App() {
   return (
     <main className="p-6 bg-purple-400 min-h-screen ">
       <Header />
-      <Switch >
-        <Route exact path="/photo/:photoId" render={()=> <Photo />} />
-        <Route exact path="/album/:albumId" render={() => <Album />} />
-        <Route exact path="/" render={() =><Albums />} />
-        <Route path="*" render={()=> <PageNotFound />} />
-      </Switch>
+      <Routes >
+        <Route path="/" element={<Albums />} />
+        <Route path="photo/:photoId" element={<Photo />} />
+        <Route path="album/:albumId" element={<Album />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
