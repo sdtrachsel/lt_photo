1625d79482069cc68a3bcce3697ccf119b40f9ac
Sharie Trachsel <122052199+sdtrachsel@users.noreply.github.com>
2023-08-09 15:00:29 -0500

    Correct api call error throw

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -18,16 +18,16 @@ export const getAlbum =(id)=>{
     })
     .then((data) => {
       if (!data || data.length === 0) {
-        throw new Error("This album doesn't exist or contains no photos.");
+        throw new Error("This album doesn't exist or contains no photos.")
       }
-      return data;
+      return data
     });
 }
 
 export const getPhoto =(id)=>{
   return fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
     .then((res) => {
-      if(res.status == 404){
+      if(res.status === 404){
         throw new Error(`Not found. This photo doesn't exist.`);
       } else if (!res.ok) {
         throw new Error(res.message);
